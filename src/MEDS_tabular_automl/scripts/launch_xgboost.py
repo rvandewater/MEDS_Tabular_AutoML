4bdc8d73ea2414dd82f5e457d4c157e7a9156269
Matthew McDermott <mattmcdermott8@gmail.com>
2024-08-11 00:29:43 -0400

    Improved xgboost error message

--- src/MEDS_tabular_automl/scripts/launch_xgboost.py
+++ src/MEDS_tabular_automl/scripts/launch_xgboost.py
@@ -195,8 +195,10 @@ class Iterator(xgb.DataIter, TimeableMixin):
         # get all window_size x aggreagation files using the file resolver
         files = get_model_files(self.cfg, self.split, self._data_shards[idx])
 
-        if not all(file.exists() for file in files):
-            raise ValueError(f"Not all files exist for shard {self._data_shards[idx]}")
+        missing_files = [file for file in files if not file.exists()]
+        if missing_files:
+            err_str = f"Missing files for shard {self._data_shards[idx]}: {missing_files}"
+            raise ValueError(err_str)
 
         dynamic_cscs = [self._load_dynamic_shard_from_file(file, idx) for file in files]
 
